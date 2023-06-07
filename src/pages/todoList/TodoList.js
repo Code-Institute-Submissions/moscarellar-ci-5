6017eba985d21bbdeb4424920566003d0a6b73e4
moscarellar <moscarellar@gmail.com>
2023-06-07 14:43:23 +0000

    fetching and add function added to todolist

--- src/pages/todoList/TodoList.js
+++ src/pages/todoList/TodoList.js
@@ -3,37 +3,69 @@ import { axiosReq } from "../../api/axiosDefaults";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
-  const [error, setError] = useState(null);
+  const [newTodo, setNewTodo] = useState("");
 
+  // Fetch the user's todos from the API
   useEffect(() => {
     const fetchTodos = async () => {
-      try {
-        const response = await axiosReq.get("/todos/");
-        setTodos(response.data.todos);
-      } catch (error) {
-        setError(error.message);
+       
+        try {
+          const { data } = await axiosReq.get(`/todos`);
+          setTodos(data.results);
+        } catch (error) {
+          console.log(error);
+        
       }
     };
 
     fetchTodos();
   }, []);
 
-  if (error) {
-    return <p>Error: {error}</p>;
-  }
+
+  
+
+  // Add a new todo
+  const addTodo = async () => {
+    try {
+        const { data } = await axiosReq.post("/todos/", {
+          title: newTodo,
+          
+        });
+        setTodos([...todos, data]);
+        setNewTodo("");
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Delete a todo
+  // const deleteTodo = async (id) => {
+  //   try {
+  //     await axiosReq.delete(`/todos/${id}`);
+  //     setTodos(todos.filter((todo) => todo.id !== id));
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   return (
     <div>
       <h2>Todo List</h2>
-      {todos && todos.length > 0 ? (
-        <ul>
-          {todos.map((todo) => (
-            <li key={todo.id}>{todo.title}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>No todos found.</p>
-      )}
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Enter a new todo"
+      />
+      <button onClick={addTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.title}{" "}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
